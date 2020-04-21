65f33ce6ce9df00a907fb882c96bb154b6ca9cdf
TomerGodelli <tomergodelli@gmail.com>
2020-04-21 18:38:32 +0300

    t1

--- js/main.js
+++ js/main.js
@@ -12,7 +12,39 @@
 		elmnt.style.backgroundColor = color;
 	}
 
+	// Get the element with id="defaultOpen" and click on it
 	$(document).ready(function() {
 		document.getElementById("defaultOpen").click();
 	});
-	// Get the element with id="defaultOpen" and click on it
+
+
+	var modal = document.getElementById("myModal");
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal 
+	btn.onclick = function () {
+		alert("test");
+		modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function () {
+		document.getElementById("myModal").style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.addEventListener("click", function(event) {
+		if (event.target == document.getElementById("myModal")) {
+			document.getElementById("myModal").style.display = "none";
+		}
+	});
+
+
+	function about() {
+		document.getElementById("myModal").style.display = "block";
+	  }
