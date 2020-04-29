8d93519cb55739af2a2e55a5d436aab3f35ad3b1
yaararm <yaararm@gmail.com>
2020-04-29 22:58:08 +0300

    yaara

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
 	// Get the element with id="defaultOpen" and click on it
 	document.getElementById("defaultOpen").click();
@@ -37,14 +36,14 @@ $(document).ready(function () {
 
 //});
 function moveToTabReg(){
-	openPage('Register', this, ' #777');
+	openPage('Register', this, ' #ca5349');
 }
 function moveToTabLogin(){
-	openPage('Login', this, ' #777');
+	openPage('Login', this, '#ca5349');
 }
 
 function switchDiv(divName){
-	openPage(divName, this, ' #777');
+	openPage(divName, this, ' #ca5349');
 
 }
 
@@ -80,3 +79,81 @@ window.onclick = function(e) {
 	if (e.target == document.getElementById("settingdialog"))
 		document.getElementById("settingdialog").close();
 };
+var leftKey;
+var rightKey
+var upKey;
+var DownKey;
+var minBalls=50;
+var maxBalls=90;
+var numOfBalls;
+var color5score;
+var color15score;
+var color25score;
+var minGhost=4;
+var maxGhost=1;
+var numOfGhost;
+var timeOfGame;
+
+
+function setLeftKey(){
+	leftKey = event.code;
+}
+function setRightKey(){
+	rightKey = event.code;
+}
+function setUpKey(){
+	upKey = event.code;
+}
+function setDownKey(){
+	DownKey = event.code;
+}
+
+$('#setting_form').validate({
+	rules: {
+		time:{
+			required: true
+		}
+	},
+	messages: {
+		time:{
+			required: "please set time for the game",
+		}
+	}
+
+});
+
+function setRandomSetting(){
+	//up down etc...
+	
+	leftKey = 37;
+	rightKey = 39;
+	upKey = 38;
+	DownKey = 40;
+	// should be random
+
+	color5score = document.getElementById("5color").value;
+	color15score = document.getElementById("15color").value;
+	color25score = document.getElementById("25color").value;
+	numOfBalls = Math.floor(Math.random() * (90 - 50 + 1) ) + 50;
+	timeOfGame = Math.floor(Math.random() * (240 - 60 + 1) ) + 60;
+	numOfGhost = Math.floor(Math.random() * (4 - 1 + 1) ) +1;
+
+
+}
+
+
+function setMySetting(){
+	// up down etc...
+
+
+	numOfBalls = document.getElementById("vol").value;
+	color5score = document.getElementById("5color").value;
+	color15score = document.getElementById("15color").value;
+	color25score = document.getElementById("25color").value;
+	timeOfGame = document.getElementById("time").value;
+	numOfGhost = document.getElementById("time").value;
+}
+
+function resetSetting(){
+
+}
